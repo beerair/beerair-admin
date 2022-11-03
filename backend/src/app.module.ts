cf34d877c8c6fccd8e2374b75892a6aae6d14b73
syoung125 <laura120555@gmail.com>
2022-11-04 01:19:09 +0900

    feat: 데이터베이스 연결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseProviderModule } from './providers/database/provider.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseProviderModule],
   controllers: [AppController],
   providers: [AppService],
 })
